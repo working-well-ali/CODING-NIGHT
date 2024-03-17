d63e41e90980c6e528d486f1246329f379474b28
ALI HAMZA NADEEM RANA <160017611+working-well-ali@users.noreply.github.com>
2024-03-18 00:10:57 +0500

    Add files via upload
    advance calculator with all operators 
    CODING NIGHT

--- top.js
+++ top.js
@@ -1,28 +1,4 @@
 import inquirer from "inquirer";
-let total = await inquirer.prompt([
-    { name: "t1", type: "number", message: "pls enter your number" },
-    { name: "t2", type: "number", message: "pls enter your number" },
-    { name: "operator", type: "list", message: "slect any one opration to perfrom action",
-        choices: ["addition", "substraction", "multiplication", "division"]
-    },
-]);
-if (total.operator === "addition") {
-    console.log(total.t1 + total.t2);
-}
-else if (total.operator === "substraction") {
-    console.log(total.t1 - total.t2);
-}
-else if (total.operator === "multiplication") {
-    console.log(total.t1 * total.t2);
-}
-else if (total.operator === "division") {
-    console.log(total.t1 / total.t2);
-}
-else
-    (console.log);
-{
-    "pls enter valid value";
-}
 let cal = await inquirer.prompt([
     {
         type: "number",
@@ -32,42 +8,101 @@ let cal = await inquirer.prompt([
     {
         type: "number",
         name: "n2",
-        message: "enter yor 2nd number"
+        message: "enter yor 2nd number",
     },
     {
         type: "number",
         name: "n3",
-        message: "enter yor 2nd number"
+        message: "enter yor 3rd number"
     },
     {
         type: 'input',
         name: 'op',
-        message: 'enter your operator:'
-    },
+        message: 'enter your operator',
+        choice: ['a/a', 'a/s', 'a/m', 'a/d']
+    }
 ]);
-if (cal.op == '+') {
+//10 + 10 + 10    
+if (cal.op == 'a/a') {
     console.log(cal.n1 + cal.n2 + cal.n3);
 }
-else if (cal.op == '-') {
+//10+10-10
+else if (cal.op == 'a/s') {
     console.log(cal.n1 + cal.n2 - cal.n3);
 }
-else if (cal.op == '*') {
+//10+10*10
+else if (cal.op == 'a/m') {
     console.log(cal.n1 + cal.n2 * cal.n3);
 }
-else if (cal.op == '/') {
+//10+10/10
+else if (cal.op == 'a/d') {
     console.log(cal.n1 + cal.n2 / cal.n3);
 }
-if (cal.op == '+') {
-    console.log(cal.n1 + cal.n2 + cal.n3);
+([{
+        type: 'input',
+        name: 'op',
+        message: 'enter your operator',
+        choice: ['sa', 'ss', 'sm', 'sd']
+    }]);
+//10-10+10
+if (cal.op == 'sa') {
+    console.log(cal.n1 - cal.n2 + cal.n3);
 }
-else if (cal.op == '-') {
-    console.log(cal.n1 + cal.n2 - cal.n3);
+//10-10-10
+else if (cal.op == 'ss') {
+    console.log(cal.n1 - cal.n2 - cal.n3);
 }
-else if (cal.op == '*') {
-    console.log(cal.n1 + cal.n2 * cal.n3);
+//10-10*10
+else if (cal.op == 'sm') {
+    console.log(cal.n1 - cal.n2 * cal.n3);
 }
-else if (cal.op == '/') {
-    console.log(cal.n1 + cal.n2 / cal.n3);
+//10-10/10
+else if (cal.op == 'sd') {
+    console.log(cal.n1 - cal.n2 / cal.n3);
+}
+([{
+        type: 'input',
+        name: 'op',
+        message: 'enter your operator',
+        choice: ['ma', 'ms', 'mm', 'md']
+    }]);
+//10*10+10      =110
+if (cal.op == 'ms') {
+    console.log(cal.n1 * cal.n2 + cal.n3);
+}
+//10*10-10    =90
+else if (cal.op == 'ms') {
+    console.log(cal.n1 * cal.n2 - cal.n3);
+}
+//10*10*10     =1000
+else if (cal.op == 'mm') {
+    console.log(cal.n1 * cal.n2 * cal.n3);
+}
+//10*10/10      =10
+else if (cal.op == 'md') {
+    console.log(cal.n1 * cal.n2 / cal.n3);
+}
+([{
+        type: 'input',
+        name: 'op',
+        message: 'enter your operator',
+        choice: ['da', 'ds', 'dm', 'dd']
+    }]);
+//10/10+10          =11
+if (cal.op == 'da') {
+    console.log(cal.n1 / cal.n2 + cal.n3);
+}
+//10/10-10      =1
+else if (cal.op == 'ds') {
+    console.log(cal.n1 / cal.n2 - cal.n3);
+}
+//10/10*10      =10
+else if (cal.op == 'dm') {
+    console.log(cal.n1 / cal.n2 * cal.n3);
+}
+//10/10/10      =0.1
+else if (cal.op == 'dd') {
+    console.log(cal.n1 / cal.n2 / cal.n3);
 }
 else {
     console.log('please enter valid numbers or operator');
